dcd3199f40eaafffc8a0d7855a2bc6967fd93e87
Chris Stalder <ChrisStalder@shearers.com>
2023-11-27 18:33:45 -0500

    Temporarily enabling developer exception page to debug login issue

--- En-Luna/Program.cs
+++ En-Luna/Program.cs
@@ -40,7 +40,8 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    //app.UseExceptionHandler("/Home/Error");
+    app.UseDeveloperExceptionPage(); // todo remove
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
